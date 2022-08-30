501177f001a8fe37455f16740653d1e637404622
Sami Hasan <shasan1@berkeley.edu>
2022-08-29 22:58:21 -0700

    Complete NFC activation logic

--- MK2Source/MK2Source.ino
+++ MK2Source/MK2Source.ino
@@ -1,52 +1,80 @@
+#include <DFRobot_BMI160.h>
 #include <SPI.h>
 #include <MFRC522.h>
 
 #define RST_PIN         9          // Configurable, see typical pin layout above
 #define SS_PIN          10         // Configurable, see typical pin layout above
+#define MINDELAY        500
+#define LED1            8
+#define BMI_I2C_ADDR    0x69
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 DFRobot_BMI160 bmi160;
-const int8_t i2c_addr = 0x69;
-int led = 8;
+
 bool armed = false;
 bool triggered = false;
 unsigned long prevReadTime = 0;
-const unsigned long minDelay = 3000;
+const String uid = "93 45 F2 94";
+String nfcData = "";
+int rslt;
+int16_t accelGyro[6] = {0};
 
 String readNFCTag(MFRC522 nfcreader) {
-  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) {
-    return "";
-  }
 
   String content = "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++)
-  {
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  
+  if ( ! nfcreader.PICC_IsNewCardPresent() || ! nfcreader.PICC_ReadCardSerial()) {
+    return content;
   }
+  
+  for (byte i = 0; i < nfcreader.uid.size; i++) {
+    content.concat(String(nfcreader.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(nfcreader.uid.uidByte[i], HEX));
+  }
+  
+  content.toUpperCase();
+  
   return content;
 }
 
 void setup() {
   Serial.begin(9600);
   while (!Serial); // Wait for connection
+  
   SPI.begin();
+  
   mfrc522.PCD_Init();
   delay(4);
   mfrc522.PCD_DumpVersionToSerial();
-  pinMode(led, OUTPUT);
+  
+  pinMode(LED1, OUTPUT);
+  
   if (bmi160.softReset() != BMI160_OK) {
     Serial.println("reset false");
     while (1);
   }
+  
   //set and init the bmi160 i2c address
-  if (bmi160.I2cInit(i2c_addr) != BMI160_OK) {
+  if (bmi160.I2cInit(BMI_I2C_ADDR) != BMI160_OK) {
     Serial.println("init false");
     while (1);
   }
 }
 
 void loop() {
-  Serial.println(readNFCTag(mfrc522));
+  rslt = bmi160.getAccelGyroData(accelGyro);
+  nfcData = readNFCTag(mfrc522);
+
+  if (millis() - prevReadTime > MINDELAY) {
+    if (nfcData.substring(1) == uid) // Make sure you change this with your own UID number
+    {
+      Serial.println(nfcData);
+    }
+  }
+
+  // Must be after all usages of 'prevReadTime'
+  if (nfcData != "") {
+    prevReadTime = millis();
+  }
 }
