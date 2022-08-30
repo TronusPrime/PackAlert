b46a573a43924c81ae5bdf69b63cf34246a46db9
Sami Hasan <shasan1@berkeley.edu>
2022-08-29 22:11:49 -0700

    Read NFC chip basic

--- MK2Source/MK2Source.ino
+++ MK2Source/MK2Source.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <MFRC522.h>
 
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial); // Wait for connection
+  SPI.begin();
+  mfrc522.PCD_Init();
+  delay(4);
+  mfrc522.PCD_DumpVersionToSerial();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
 
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
