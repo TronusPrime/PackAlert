0e7c644c34f038f6222fbaa48fca7c7fb218c911
Sami Hasan <shasan1@berkeley.edu>
2022-08-29 22:19:48 -0700

    Add nfc reader function

--- MK2Source/MK2Source.ino
+++ MK2Source/MK2Source.ino
@@ -6,6 +6,21 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
+String readNFCTag(MFRC522 nfcreader) {
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) {
+    return "";
+  }
+
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  return content;
+}
+
 void setup() {
   Serial.begin(9600);
   while (!Serial); // Wait for connection
@@ -16,16 +31,5 @@ void setup() {
 }
 
 void loop() {
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  // Dump debug info about the card; PICC_HaltA() is automatically called
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  Serial.println(readNFCTag(mfrc522));
 }
